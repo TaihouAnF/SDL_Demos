39c04f81a78f5674eaa887f07ee025b89da8bf65
Taihou F <59370291+AnsonFeng1999@users.noreply.github.com>
2023-10-18 18:25:18 -0400

    Finish setting up picture using SDL

--- First-demo/01_hello_SDL.cpp
+++ First-demo/01_hello_SDL.cpp
@@ -1,56 +1,113 @@
-/*This source code copyrighted by Lazy Foo' Productions 2004-2023
-and may not be redistributed without written permission.*/
-
-//Using SDL and standard IO
+// Using SDL and standard IO
 #include <SDL.h>
 #include <stdio.h>
+#include <iostream>
 
-//Screen dimension constants
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
+// Screen dimension constants
+const int screen_width = 2149;
+const int screen_height = 1175;
+
+// Starts SDL
+bool init();
+// Load Media
+bool loadMedia();
+// Free Memory and 
+void close();
+
+// The window we'll be rendering to
+SDL_Window* window = NULL;
+// The surface/canvas we put into the window
+SDL_Surface* screenSurface = NULL;
+// The image we load 
+SDL_Surface* image = NULL;
+
+bool init() {
+	// A flag indicating initialization
+	bool success = true;
+
+	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+		std::cout << "SDL failed to initialize, exit with code: " << SDL_GetError() << ".\n";
+		success = false;
+	}
+	else {
+		window = SDL_CreateWindow("My image", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, screen_width, screen_height, SDL_WINDOW_SHOWN);
+		if (window == NULL) {
+			std::cout << "Window cannot be created, exit with code: " << SDL_GetError() << ".\n";
+			success = false;
+		}
+		else {
+			screenSurface = SDL_GetWindowSurface(window);
+		}
+	}
+	return success;
+}
+
+bool loadMedia() {
+	// Similar to init(), we need a flag here
+	bool success = true;
+
+	image = SDL_LoadBMP("temp/test.bmp");
+	if (image == NULL) {
+		std::cout << "SDL failed to load BMP, exit with code: " << SDL_GetError() << ".\n";
+		success = false;
+	}
+	
+	return success;
+}
+
+void close() {
+	
+	// Deallocate surface
+	SDL_FreeSurface(image);
+	image = NULL;
+
+	// Don't worry about the surface here, Destroy window would take care of it
+	/*SDL_FreeSurface(screenSurface);
+	screenSurface = NULL;*/
+
+	// Clean up window
+	SDL_DestroyWindow(window);
+	window = NULL;
+	screenSurface = NULL;
+
+	SDL_Quit();
+}
 
 int main( int argc, char* args[] )
 {
-	//The window we'll be rendering to
-	SDL_Window* window = NULL;
-	
-	//The surface contained by the window
-	SDL_Surface* screenSurface = NULL;
-
-	//Initialize SDL
-	if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
-	{
-		printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
+	// Boot the exe
+	if (!init()) {
+		printf("Failed to Initialize.\n");
 	}
-	else
-	{
-		//Create window
-		window = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
-		if( window == NULL )
-		{
-			printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
+	else {
+		if (!loadMedia()) {
+			printf("Failed to Load media.\n");
 		}
-		else
-		{
-			//Get window surface
-			screenSurface = SDL_GetWindowSurface( window );
-
-			//Fill the surface white
-			SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xFF, 0xFF, 0xFF ) );
+		else {
+			SDL_BlitSurface(image, NULL, screenSurface, NULL);
+/**
+	By LazyFoo :
+	By default, most rendering systems out there are double buffered. These two buffers are the front and back buffer.
+	When you make draw calls like SDL_BlitSurface, you render to the back buffer. What you see on the screen is the front buffer. 
+	The reason we do this is because most frames require drawing multiple objects to the screen. 
+	If we only had a front buffer, we would be able to see the frame as things are being drawn to it which means we would see unfinished frames. 
+	So what we do is draw everything to the back buffer first and once we're done we swap the back and front buffer so now the user can see the finished frame.
+*/
+			SDL_UpdateWindowSurface(window);
 			
-			//Update the surface
-			SDL_UpdateWindowSurface( window );
-            
-            //Hack to get window to stay up
-            SDL_Event e; bool quit = false; while( quit == false ){ while( SDL_PollEvent( &e ) ){ if( e.type == SDL_QUIT ) quit = true; } }
+			// Keep the window up
+			SDL_Event event;
+			bool quit = false;
+			while (!quit) {
+				while (SDL_PollEvent(&event)) {
+					if (event.type == SDL_QUIT) {
+						quit = true;
+					}
+				}
+			}
 		}
 	}
 
-	//Destroy window
-	SDL_DestroyWindow( window );
-
-	//Quit SDL subsystems
-	SDL_Quit();
-
+	close();
 	return 0;
 }
